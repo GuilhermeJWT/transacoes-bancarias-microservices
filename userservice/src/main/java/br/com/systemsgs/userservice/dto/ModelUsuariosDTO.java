117a093d011744dbbf20f48a8f8ec80ab5a6ce81
GuilhermeJWT <guiromanno@gmail.com>
2024-07-26 18:49:42 -0300

    UPDATE: Altera Dtos

--- userservice/src/main/java/br/com/systemsgs/userservice/dto/ModelUsuariosDTO.java
+++ userservice/src/main/java/br/com/systemsgs/userservice/dto/ModelUsuariosDTO.java
@@ -5,34 +5,37 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.persistence.EnumType;
 import jakarta.persistence.Enumerated;
 import jakarta.validation.constraints.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import org.hibernate.validator.constraints.br.CPF;
 
 import java.math.BigDecimal;
 
-public record ModelUsuariosDTO
-
-        (
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class ModelUsuariosDTO {
 
         @NotBlank(message = "Nome Obrigatório!")
-        String nome,
+        private String nome;
 
         @NotBlank(message = "Email Obrigatório!")
         @Email(message = "Formato de E-mail Inválido!")
-        String email,
+        private String email;
 
         @CPF(message = "Formato de Cpf Inválido!")
         @NotBlank(message = "CPF Obrigatório")
-        String cpf,
+        private String cpf;
 
         @NotNull(message = "Informe o Valor.")
         @DecimalMin(value = "0.0", inclusive = false, message = "O preço deve ser maior que 0.")
         @DecimalMax(value = "999999.0", message = "Valor muito alto para a conta.")
         @JsonProperty("valor_conta")
-        BigDecimal valorConta,
+        private BigDecimal valorConta;
 
         @NotNull(message = "Informe o tipo de Carteira - USUARIO_COMUM ou LOJISTA")
         @Enumerated(EnumType.STRING)
         @JsonProperty("tipo_carteira")
-        TipoCarteira tipoCarteira
-
-        ){}
+        private TipoCarteira tipoCarteira;
+}
