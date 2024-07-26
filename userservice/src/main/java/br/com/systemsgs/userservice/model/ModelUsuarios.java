dcee18f6030b1921d7bc450035c98800af948956
GuilhermeJWT <guiromanno@gmail.com>
2024-07-26 16:55:28 -0300

    ADD: ModelUsuarios

---
+++ userservice/src/main/java/br/com/systemsgs/userservice/model/ModelUsuarios.java
@@ -0,0 +1,56 @@
+package br.com.systemsgs.userservice.model;
+
+import br.com.systemsgs.userservice.enums.TipoCarteira;
+import jakarta.persistence.*;
+import lombok.*;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.io.Serializable;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "tbl_usuarios")
+@SequenceGenerator(name = "id_gen_usuarios", sequenceName = "usuarios_seq", initialValue = 1, allocationSize = 1)
+public class ModelUsuarios implements Serializable {
+
+    @Id
+    @GeneratedValue(generator = "id_gen_usuarios", strategy = GenerationType.AUTO)
+    private Long id;
+
+    @Column(name = "nome_usuario", length = 150)
+    private String nome;
+
+    @Column(name = "email", unique = true, length = 150)
+    private String email;
+
+    @Column(name = "senha", length = 100)
+    private String senha;
+
+    @Column(name = "cpf", unique = true, length = 20)
+    private String cpf;
+
+    @Column(name = "valor_na_conta")
+    private BigDecimal valorConta = BigDecimal.ZERO;
+
+    @Column(name = "tipo_carteira")
+    @Enumerated(EnumType.STRING)
+    private TipoCarteira tipoCarteira;
+
+    @Column(name = "quantidade_transacoes_realizadas")
+    private Integer quantidadeTransacoesRealizadas;
+
+    @Column(name = "total_transacoes_realizadas")
+    private BigDecimal totalTransacoesRealizadas;
+
+    @Column(name = "data_criacao_usuario")
+    @CreationTimestamp
+    private LocalDateTime dataCriacao;
+
+    @Column(name = "data_ultima_alteracao")
+    @CreationTimestamp
+    private LocalDateTime dataAlteracao;
+}
