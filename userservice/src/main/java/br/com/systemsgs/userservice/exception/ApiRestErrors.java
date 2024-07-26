82974317a6882b2cf1418bf6f82c8da4e972c52b
GuilhermeJWT <guiromanno@gmail.com>
2024-07-26 17:37:32 -0300

    ADD: Camada de Exception

---
+++ userservice/src/main/java/br/com/systemsgs/userservice/exception/ApiRestErrors.java
@@ -0,0 +1,21 @@
+package br.com.systemsgs.userservice.exception;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ApiRestErrors {
+
+    private List<String> erros;
+
+    public ApiRestErrors(List<String> erros) {
+        this.erros = erros;
+    }
+
+    public ApiRestErrors(String mensagemErro){
+        this.erros = Arrays.asList(mensagemErro);
+    }
+
+    public List<String> getErros() {
+        return erros;
+    }
+}
---
+++ userservice/src/main/java/br/com/systemsgs/userservice/exception/RestControllerAdviceException.java
@@ -0,0 +1,30 @@
+package br.com.systemsgs.userservice.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.MethodArgumentNotValidException;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestControllerAdvice
+public class RestControllerAdviceException {
+
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiRestErrors handlerMethodNotValidException(MethodArgumentNotValidException methodArgumentNotValidException){
+        List<String> errors =  methodArgumentNotValidException.getBindingResult().getAllErrors()
+                .stream().map(erro -> erro.getDefaultMessage())
+                .collect(Collectors.toList());
+
+        return new ApiRestErrors(errors);
+    }
+
+    @ExceptionHandler(UsuarioNaoEncontradoException.class)
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    public ApiRestErrors usuarioNaoEncontradoException(){
+        return new ApiRestErrors(new UsuarioNaoEncontradoException().getMessage());
+    }
+}
