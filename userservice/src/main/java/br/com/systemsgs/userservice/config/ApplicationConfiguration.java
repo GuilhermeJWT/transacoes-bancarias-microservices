abd9d03a6b8619eccb0226941b6689af6f9bc8ad
GuilhermeJWT <guiromanno@gmail.com>
2024-08-01 20:44:36 -0300

    ADD: Conexão - RabbitMq

--- userservice/src/main/java/br/com/systemsgs/userservice/config/ApplicationConfiguration.java
+++ userservice/src/main/java/br/com/systemsgs/userservice/config/ApplicationConfiguration.java
@@ -1,5 +1,8 @@
 package br.com.systemsgs.userservice.config;
 
+import org.springframework.amqp.core.Declarable;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.modelmapper.ModelMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -7,8 +10,20 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ApplicationConfiguration {
 
+    public static final String QUEUE_TRANSACATION = "queue-transaction";
+
     @Bean
     public ModelMapper mapper(){
         return new ModelMapper();
+    }
+
+    @Bean
+    public Declarable transactionQueue(){
+        return new Queue(QUEUE_TRANSACATION);
+    }
+
+    @Bean
+    public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
+        return new Jackson2JsonMessageConverter();
     }
 }
