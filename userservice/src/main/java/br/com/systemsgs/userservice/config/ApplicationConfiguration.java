eee81aaa02b0c77dda61ecf06961a662c273aabb
GuilhermeJWT <guiromanno@gmail.com>
2024-08-05 13:20:04 -0300

    UPDATE: Config RabbitMq

--- userservice/src/main/java/br/com/systemsgs/userservice/config/ApplicationConfiguration.java
+++ userservice/src/main/java/br/com/systemsgs/userservice/config/ApplicationConfiguration.java
@@ -1,9 +1,12 @@
 package br.com.systemsgs.userservice.config;
 
-import org.springframework.amqp.core.Declarable;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.modelmapper.ModelMapper;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -18,8 +21,18 @@ public class ApplicationConfiguration {
     }
 
     @Bean
-    public Declarable transactionQueue(){
-        return new Queue(QUEUE_TRANSACATION);
+    public Queue queue(){
+        return new Queue("queue-transaction");
+    }
+
+    @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
     }
 
     @Bean
