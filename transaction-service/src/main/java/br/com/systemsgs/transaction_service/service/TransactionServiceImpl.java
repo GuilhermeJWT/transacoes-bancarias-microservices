ad5a69785eaf057e2928d29ca1fe08f894b20e75
GuilhermeJWT <guiromanno@gmail.com>
2024-08-06 15:03:27 -0300

    ADD: Salvando uma Transação Aprovada no MongoDB

--- transaction-service/src/main/java/br/com/systemsgs/transaction_service/service/TransactionServiceImpl.java
+++ transaction-service/src/main/java/br/com/systemsgs/transaction_service/service/TransactionServiceImpl.java
@@ -1,19 +1,54 @@
 package br.com.systemsgs.transaction_service.service;
 
 import br.com.systemsgs.transaction_service.dto.PayloadTransacaoRequestRabbitMq;
+import br.com.systemsgs.transaction_service.enums.StatusPedidoTransacao;
 import br.com.systemsgs.transaction_service.enums.TipoCarteira;
 import br.com.systemsgs.transaction_service.exception.erros.TransacaoNegadaException;
+import br.com.systemsgs.transaction_service.model.ModelTransaction;
+import br.com.systemsgs.transaction_service.repository.TransactionRepository;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.UUID;
 
 @Service
 public class TransactionServiceImpl {
 
-    public void processaValidacaoPedidoTransacao(PayloadTransacaoRequestRabbitMq payloadMessage){
+    private final TransactionRepository transactionRepository;
+    private final RabbitTemplate rabbitTemplate;
 
+    @Autowired
+    public TransactionServiceImpl(TransactionRepository transactionRepository, RabbitTemplate rabbitTemplate) {
+        this.transactionRepository = transactionRepository;
+        this.rabbitTemplate = rabbitTemplate;
+    }
+
+    @Transactional
+    public void processaValidacaoPedidoTransacao(PayloadTransacaoRequestRabbitMq payloadMessage){
         if(!payloadMessage.getTipoCarteira().equals(TipoCarteira.USUARIO_COMUM)) {
-             System.out.println("deu certo");
+
+            var transacaoSalva =  salvaTransacaoAprovada(payloadMessage);
+
         }else {
             throw new TransacaoNegadaException();
         }
     }
+
+    private ModelTransaction salvaTransacaoAprovada(PayloadTransacaoRequestRabbitMq payloadMessage){
+        ModelTransaction modelTransaction = new ModelTransaction();
+
+        modelTransaction.setIdTransacao(UUID.randomUUID());
+        modelTransaction.setIdPagador(payloadMessage.getIdPagador());
+        modelTransaction.setNomePagador(payloadMessage.getNomePagador());
+        modelTransaction.setIdBeneficiario(payloadMessage.getIdBeneficiario());
+        modelTransaction.setNomeBeneficiario(payloadMessage.getNomeBeneficiario());
+        modelTransaction.setEmailBeneficiario(payloadMessage.getEmailBeneficiario());
+        modelTransaction.setValorTransferencia(payloadMessage.getValorTransferencia());
+        modelTransaction.setTipoCarteira(StatusPedidoTransacao.AUTORIZADA.name());
+
+        return transactionRepository.save(modelTransaction);
+    }
+
 }
