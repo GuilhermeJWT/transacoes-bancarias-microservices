c783b5f5775be26629822902a94c899ddf7899c1
GuilhermeJWT <guiromanno@gmail.com>
2024-08-06 14:34:25 -0300

    UPDATE: Configurações RabbitMq

--- transaction-service/src/main/java/br/com/systemsgs/transaction_service/config/RabbitMqConfiguration.java
+++ transaction-service/src/main/java/br/com/systemsgs/transaction_service/config/RabbitMqConfiguration.java
@@ -2,7 +2,12 @@ package br.com.systemsgs.transaction_service.config;
 
 import org.springframework.amqp.core.Declarable;
 import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.connection.ConnectionFactory;
+import org.springframework.amqp.rabbit.core.RabbitAdmin;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.context.ApplicationListener;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -10,6 +15,9 @@ import org.springframework.context.annotation.Configuration;
 public class RabbitMqConfiguration {
 
     public static final String QUEUE_TRANSACTION = "queue-transaction";
+    public static final String QUEUE_TRANSACTION_NEGADA = "queue-transaction-negada";
+    public static final String QUEUE_TRANSACTION_APROVADA = "queue-transaction-aprovada";
+    public static final String QUEUE_NOTIFICATION = "queue-notification";
 
     @Bean
     public Declarable orderQueue(){
@@ -17,7 +25,25 @@ public class RabbitMqConfiguration {
     }
 
     @Bean
+    public RabbitAdmin rabbitAdmin(ConnectionFactory connectionFactory){
+        return new RabbitAdmin(connectionFactory);
+    }
+
+    @Bean
+    public ApplicationListener<ApplicationReadyEvent> applicationReadyEventApplicationListener(RabbitAdmin rabbitAdmin){
+        return event -> rabbitAdmin.initialize();
+    }
+
+    @Bean
     public Jackson2JsonMessageConverter jackson2JsonMessageConverter(){
         return new Jackson2JsonMessageConverter();
+    }
+
+    @Bean
+    public RabbitTemplate rabbitTemplate(ConnectionFactory connectionFactory, Jackson2JsonMessageConverter jacksonConverter){
+        RabbitTemplate rabbitTemplate = new RabbitTemplate(connectionFactory);
+        rabbitTemplate.setMessageConverter(jacksonConverter);
+
+        return rabbitTemplate;
     }
 }
