24a5ab8214717959538af55e8d87e814ae0883eb
GuilhermeJWT <guiromanno@gmail.com>
2024-08-09 22:19:46 -0300

    UPDATE: Ajusta msg de Erro

--- transaction-service/src/main/java/br/com/systemsgs/transactionservice/exception/erros/TransacaoNegadaException.java
+++ transaction-service/src/main/java/br/com/systemsgs/transactionservice/exception/erros/TransacaoNegadaException.java
@@ -3,7 +3,6 @@ package br.com.systemsgs.transactionservice.exception.erros;
 public class TransacaoNegadaException extends RuntimeException{
 
     public TransacaoNegadaException(){
-        super("Transação Negada! apenas Usuários Comuns podem Transferir Dinheiro.");
+        super("Transação Negada! apenas Usuários Comuns podem Transferir Dinheiro!");
     }
-
 }
